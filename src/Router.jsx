5fc12aba06635876e669fbfaef3126cbbe4dfbce
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-05-30 21:47:28 +0200

    update routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Login from "./Login";
+import Edit from "./Edit";
+import Delete from "./Delete";
+import Create from "./Create";
+import Read from "./Read";
 
 const router = createBrowserRouter([
   {
@@ -10,7 +14,12 @@ const router = createBrowserRouter([
   {
     path: "/dashboard",
     element: <App />,
-    children: [],
+    children: [
+      { path: "/dashboard/:id", element: <Read /> },
+      { path: "/dashboard/", index: true, element: <Create /> },
+      { path: "/dashboard/:id/edit", element: <Edit /> },
+      { path: "/dashboard/:id/delete", element: <Delete /> },
+    ],
   },
 ]);
 export default router;
